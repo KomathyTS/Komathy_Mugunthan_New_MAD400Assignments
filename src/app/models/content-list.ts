968340396629fff66d3a21d40bdd1f614bd2112b
Komathy Mugunthan <samokst@yahoo.com>
2023-01-29 00:04:06 -0500

    getter method returns IContent array, and the method will returns the length of the IContent array

--- src/app/models/content-list.ts
+++ src/app/models/content-list.ts
@@ -1,2 +1,24 @@
 export class ContentList {
+
+    static contentCount = 0; 
+    _addContent: IContent[]; 
+
+    constructor(addContent: IContent) {
+        this._addContent = []; 
+        this._addContent[0] = addContent; 
+        this.increaseCount();
+          }
+          increaseCount() {
+            return ++ContentList.contentCount;
+        }
+
+    private _addContent: IContent[];
+
+    get addContent(): IContent[]{ 
+
+        return this._addContent;
+
+    }
 }
+    
+    
