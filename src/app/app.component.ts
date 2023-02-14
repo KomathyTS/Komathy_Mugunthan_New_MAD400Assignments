de2cabd737e3ccd5cdb79c991369413d14fb760e
Komathy Mugunthan <samokst@yahoo.com>
2023-02-13 22:26:44 -0500

    Added Content List tag in the AppComponent Created 4 array of IContent items in the Content List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'K_Mugunthan_CHessGames';
+
+  constructor() {
+
+  }
+  
 }
--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IContent } from '../models/icontent';
 
 @Component({
   selector: 'app-content-list',
@@ -7,5 +8,47 @@ import { Component } from '@angular/core';
 })
 
 export class ContentListComponent {
+
+  chessGameArray: IContent[];
+
+  constructor() {
+    this.chessGameArray = [{
+
+       id: 0,
+       title: "toytheater",
+       description: "Offers educational math, literacy, and art activities and games",
+       author: "Ben KG",
+       imgSrc: "https://toytheater.com/wp-content/uploads/chess.jpg",
+       type: "Online chess game",
+       tags: ["Person VS Person"]
+       },
+       {
+       id: 1,
+       title: "chesskid",
+       description: "Is to make chess fun, safe and accessible",
+       author: "FunMasterMike",
+       imgSrc: "https://www.chesskid.com/images/socialmedia/welcome_to_chesskid.png",
+       type: "Online chess game",
+       tags: ["Learn"]
+       },
+       {
+        id: 2,
+        title: "chessmatec",
+        description: "The ChessMatec App is an interactive educational game, the most advanced one for learning and practicing the game of chess in a fun and exciting way",
+        author: "Dana Lazarof",
+        imgSrc: "https://play-lh.googleusercontent.com/SX2NTZLFLUXSIK36dN19uzmqZCMoqUPx4l74D-nxAK71o4YXL8JabA_xjgu8-dH7Jw=w526-h296-rw",
+        type: "Chess app",
+        tags: ["Learn"]
+       },
+       {
+        id: 3,
+        title: "lichess",
+        description: " Play chess in a clean interface. No registration, no ads, no plugin required.",
+        author: "Thibault Duplessis",
+        imgSrc: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Lichess_Logo_2019.svg/1200px-Lichess_Logo_2019.svg.png",
+        type: "Online chess",
+        tags: ["Rapid"]
+    }]
+  }
 
 }
