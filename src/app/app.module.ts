1185624f795504d4a1769544b45adb71bb1a1e17
Komathy Mugunthan <samokst@yahoo.com>
2023-02-15 18:17:09 -0500

    -Changed package.jase from 2 to 3 -Took IContent array from ContentList component to data/ data.content.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentListItemComponent } from './content-list-item/content-list-item.component';
+import { DataComponent } from './data/data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentListComponent,
-    ContentListItemComponent
+    ContentListItemComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/data/data.component.ts
@@ -0,0 +1,40 @@
+import { IContent } from '../models/icontent';
+
+const CHESSGAMES: IContent[] = [{
+
+     id: 0,
+     title: "toytheater",
+     description: "Offers educational math, literacy, and art activities and games",
+     author: "Ben KG",
+     imgSrc: "https://toytheater.com/wp-content/uploads/chess.jpg",
+     type: "Online chess game",
+     tags: ["Person VS Person", "Person VS Computer"]
+     },
+     {
+     id: 1,
+     title: "chesskid",
+     description: "Is to make chess fun, safe and accessible",
+     author: "FunMasterMike",
+     imgSrc: "https://www.chesskid.com/images/socialmedia/welcome_to_chesskid.png",
+     type: "Online chess game",
+     tags: ["Learn", "Puzzels", "Play", "Coaching"]
+     },
+     {
+      id: 2,
+      title: "chessmatec",
+      description: "The ChessMatec App is an interactive educational game, the most advanced one for learning and practicing the game of chess in a fun and exciting way",
+      author: "Dana Lazarof",
+      imgSrc: "https://play-lh.googleusercontent.com/SX2NTZLFLUXSIK36dN19uzmqZCMoqUPx4l74D-nxAK71o4YXL8JabA_xjgu8-dH7Jw=w526-h296-rw",
+      type: "Chess app",
+      tags: ["Learn", "Chess App", "Play", "Videos"]
+     },
+     {
+      id: 3,
+      title: "lichess",
+      description: " Play chess in a clean interface. No registration, no ads, no plugin required.",
+      author: "Thibault Duplessis",
+      imgSrc: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Lichess_Logo_2019.svg/1200px-Lichess_Logo_2019.svg.png",
+      type: "Online chess",
+      tags: ["Rapid", "Blitz", "Bullet", "Classical", "Custom"]
+  }];
+
