078afd82cc2a23e40cf27ccca5a15ce82e98a1b5
Komathy Mugunthan <samokst@yahoo.com>
2023-02-16 20:23:53 -0500

    - Made adjustment in the data/mock-content.ts - Created the service name called chess-player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentListItemComponent } from './content-list-item/content-list-item.component';
-import { DataComponent } from './data/data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentListComponent,
-    ContentListItemComponent,
-    DataComponent
+    ContentListItemComponent
   ],
   imports: [
     BrowserModule
