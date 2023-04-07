4dee1ed214ff2018ecbee417848fdc8cca415ede
Komathy Mugunthan <samokst@yahoo.com>
2023-04-06 20:04:53 -0400

    Completed step 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ContentListComponent } from './content-list/content-list.component';
+import { ContentListItemDetailComponent } from './content-list-item-detail/content-list-item-detail.component';
+
+const routes: Routes = [
+  {
+    path: "content",
+    component: ContentListComponent,
+  },
+  {
+    path: "",
+    redirectTo: "/content",
+    pathMatch: "full"
+  },
+  {
+    path: "content/:idNumberValue",
+    component: ContentListItemDetailComponent
+  }
+];
 
 
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [ 
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+   exports: [RouterModule]
+  
 })
 export class AppRoutingModule { }
