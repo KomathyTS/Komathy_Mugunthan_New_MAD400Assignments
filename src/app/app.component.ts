fb181e5519d78b244f4c9b7f56db464ba39cc186
Komathy Mugunthan <samokst@yahoo.com>
2023-04-08 15:40:06 -0400

    Switch from appCompontent to serch component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,11 @@
-import { Component,  OnInit } from '@angular/core';
-import { IContent } from './models/icontent';
-import { ChessPlayerService } from './service/chess-player.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  // singleGame: IContent | undefined;
-  singleGame?: IContent;
+export class AppComponent {
   title = 'K_Mugunthan_CHessGames';
 
-  constructor(private chessPlayerService: ChessPlayerService) {
-  }
-
-  ngOnInit(): void{
-    this.getTheNewItem("0");
-   }
-   getTheNewItem(newIdNumber: string): void {
-     console.warn("Getting an item: ", newIdNumber);
-     
-    this.chessPlayerService.getContentItem(Number(newIdNumber)).subscribe((chessGame: IContent) => {
-      console.warn("Got the input item ", chessGame);
-       this.singleGame = chessGame;
-    });
-  }
-  
-  
 }
--- src/app/content-list-item-search/content-list-item-search.component.ts
+++ src/app/content-list-item-search/content-list-item-search.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IContent } from '../models/icontent';
+import { ChessPlayerService } from '../service/chess-player.service';
+
 
 @Component({
   selector: 'app-content-list-item-search',
   templateUrl: './content-list-item-search.component.html',
   styleUrls: ['./content-list-item-search.component.scss']
 })
-export class ContentListItemSearchComponent {
+export class ContentListItemSearchComponent implements OnInit {
+  singleGame?: IContent;
 
+  constructor(private chessPlayerService: ChessPlayerService) {
+
+  }
+
+  ngOnInit(): void {
+    this.chessPlayerService.getContentItem(2).subscribe((chessGame: IContent) => {
+      console.warn("Got the item: ", chessGame);
+      this.singleGame = chessGame
+    });
+  
+  }
 }
