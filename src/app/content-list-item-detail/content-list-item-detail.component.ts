4b03ea89451e017427cdc23ce4f37b4fa3e76eb1
Komathy Mugunthan <samokst@yahoo.com>
2023-04-13 19:38:15 -0400

    In detail added OnInit, imported, done html & css

--- src/app/content-list-item-detail/content-list-item-detail.component.ts
+++ src/app/content-list-item-detail/content-list-item-detail.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IContent } from '../models/icontent';
+import { ChessPlayerService } from '../service/chess-player.service';
 
 @Component({
   selector: 'app-content-list-item-detail',
   templateUrl: './content-list-item-detail.component.html',
   styleUrls: ['./content-list-item-detail.component.scss']
 })
-export class ContentListItemDetailComponent {
+export class ContentListItemDetailComponent implements OnInit{
+  id?: number;
+  chessGameDetailItem?: IContent
 
+  constructor(private route: ActivatedRoute, private chessPlayerService: ChessPlayerService) {
+
+  }
+
+  ngOnInit(){
+    this.route.paramMap.subscribe(params => {
+      this.id = Number(params.get('idNumberValue'))
+
+
+      this.chessPlayerService.getContentItem(this.id).subscribe((chessGame: IContent) => {
+        console.warn("Got the item in the detail: ", chessGame);
+        this.chessGameDetailItem = chessGame;
+      });
+    });
+  }
 }
