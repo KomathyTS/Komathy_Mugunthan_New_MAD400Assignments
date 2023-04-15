665288781d7f18462dafbbea572a955fa50b267b
Komathy Mugunthan <samokst@yahoo.com>
2023-04-15 15:15:16 -0400

    Deleted the testGame, update & delete functions

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -26,26 +26,26 @@ export class ContentListComponent implements OnInit {
       console.log("Testing getting a single content item: ", chessGame);
     }); 
 
-    let testGameToUpdate: IContent = {
-      id: 2,
-      title: "365Chess",
-      description: "This has the biggest searchable chess games database online.",
-      author: "arla Faraguna",
-      imgSrc: "https://images.chesscomfiles.com/uploads/v1/landing/358.d7e3e8d3.1052x1052o.2ed81ac505ec.jpg",
-      type: "Online chess game",
-    };
+    // let testGameToUpdate: IContent = {
+    //   id: 2,
+    //   title: "365Chess",
+    //   description: "This has the biggest searchable chess games database online.",
+    //   author: "arla Faraguna",
+    //   imgSrc: "https://images.chesscomfiles.com/uploads/v1/landing/358.d7e3e8d3.1052x1052o.2ed81ac505ec.jpg",
+    //   type: "Online chess game",
+    // };
 
-    // this.chessPlayerService.addContentItem(testGameToAdd).subscribe((chessGame: IContent[]) => {
-    //   console.log("Testing adding a game to the array: ", chessGame);
+    // // this.chessPlayerService.addContentItem(testGameToAdd).subscribe((chessGame: IContent[]) => {
+    // //   console.log("Testing adding a game to the array: ", chessGame);
+    // // });
+
+    // this.chessPlayerService.updateContentItem(testGameToUpdate).subscribe((chessGame: IContent[]) => {
+    //   console.log("Testing updating a game in the array: ", chessGame);
     // });
 
-    this.chessPlayerService.updateContentItem(testGameToUpdate).subscribe((chessGame: IContent[]) => {
-      console.log("Testing updating a game in the array: ", chessGame);
-    });
-
-    this.chessPlayerService.deleteContentItem(0).subscribe((chessGame: IContent) => {
-      console.log("Testing deleting a single item (goose game): ", chessGame);
-    });
+    // this.chessPlayerService.deleteContentItem(0).subscribe((chessGame: IContent) => {
+    //   console.log("Testing deleting a single item (goose game): ", chessGame);
+    // });
 
   }
 
