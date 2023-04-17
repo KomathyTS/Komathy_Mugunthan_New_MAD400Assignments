f41deed1051f886fc18a9da96affd2a1de7c582d
Komathy Mugunthan <samokst@yahoo.com>
2023-04-17 19:50:13 -0400

    created createDb() & created in-memory-web-api

--- src/app/service/in-memory-data.service.ts
+++ src/app/service/in-memory-data.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { CHESSGAMES } from '../data/mock-content';
+import { IContent } from '../models/icontent';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDataService {
 
   constructor() { }
+
+  createDb() {
+    let games: IContent[] = CHESSGAMES;
+    return { games };
+  }
 }
