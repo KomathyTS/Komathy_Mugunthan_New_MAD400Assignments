c8b9cd36bf13d32b3cab54dffbb91fffc951e6cc
Komathy Mugunthan <samokst@yahoo.com>
2023-04-19 11:16:54 -0400

    When click the button it will added new Item.

--- src/app/modify-content/modify-content.component.ts
+++ src/app/modify-content/modify-content.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { INVALIDGAME } from '../data/mock-content';
 import { IContent } from '../models/icontent';
+import { ChessPlayerService } from '../service/chess-player.service';
 
 @Component({
   selector: 'app-modify-content',
@@ -8,6 +9,32 @@ import { IContent } from '../models/icontent';
   styleUrls: ['./modify-content.component.scss']
 })
 export class ModifyContentComponent {
-  newContentItem: IContent = INVALIDGAME; 
-  tagsToBeParsed: string = "";
+  newContentItem!: IContent; 
+  tagsToBeParsed!: string;
+  contentAddedSucessfully = false;
+
+  constructor(private chessPlayerService: ChessPlayerService) {
+    this.resetContent();
+  }
+
+  addContent(): void {
+    this.newContentItem.tags = 
+    this.tagsToBeParsed.split(",").map(tag => 
+      tag.trim()).filter(tag => tag.length > 0);
+
+      //************************************************************ */
+      this.newContentItem.id = 0;
+
+      this.chessPlayerService.addContentItem(this.newContentItem).subscribe(newItemFromServer => {
+        console.log("Chess Game Sucessfully Added", newItemFromServer);
+        this.contentAddedSucessfully = true;
+        this.resetContent();
+      });
+  }
+
+  resetContent(): void {
+    this.newContentItem = structuredClone(INVALIDGAME);
+    this.tagsToBeParsed = "";
+  }
+
 }
